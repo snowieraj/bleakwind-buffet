9288d4eff3858b2fc5725acb6d26be4530f16462
Andrew Yi <yia@ksu.edu>
2020-10-30 18:08:47 -0500

    Mileston Web Complete

---
+++ WebsiteTests/SiteTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using Website.Pages;
+using Website.Pages.Shared;
+using BleakwindBuffet.Data.Menu;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Entrees;
+
+namespace WebsiteTests
+{
+    public class SiteTests
+    {
+        [Fact]
+        public void SiteShouldContain()
+        {
+            
+        }
+    }
+}
